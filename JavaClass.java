f3b945e9135dd726567444745a20735f374c90b0
manojyadav52 <122738080+manojyadav52@users.noreply.github.com>
2023-02-10 00:03:16 +0530

    Update JavaClass.java

--- JavaClass.java
+++ JavaClass.java
@@ -4,6 +4,7 @@ public static void main(String[] args)
 {
   System.out.print("Git First Push");
   System.out.println("Hello java Developrs");
+   System.out.println("Hello java Developrs");
 
 }
 }
